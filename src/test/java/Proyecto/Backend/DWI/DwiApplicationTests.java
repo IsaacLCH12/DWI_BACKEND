0fbe8263280e7aaa891e4779ee2161176ecdf63c
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-04-16 17:41:48 -0500

    Creacion de la base Backend del proyecto

---
+++ src/test/java/Proyecto/Backend/DWI/DwiApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto.Backend.DWI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
