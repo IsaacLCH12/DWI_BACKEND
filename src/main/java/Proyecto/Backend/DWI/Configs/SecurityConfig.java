8514f9dc19e81e3985f2498dae8eddb3b075a23f
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-09 01:01:40 -0500

    fix: arreglar lo de medicos 5

--- src/main/java/Proyecto/Backend/DWI/Configs/SecurityConfig.java
+++ src/main/java/Proyecto/Backend/DWI/Configs/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                                 "/api/sedes/activas",
                                 "/api/servicios/activos",
                                 "/api/medicos/filtrar",
-                                "api/medicos",
+                                "/api/medicos",
                                 "/api/medicos/activos",
                                 "/api/horarios/medico/**")
                         .permitAll()
